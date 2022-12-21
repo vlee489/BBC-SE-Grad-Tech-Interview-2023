55139a01c4f8e3e7ad52b6d1000c1b2b2b5d0bd9
Vincent Lee <vlee@vlee.me.uk>
2022-12-21 16:30:57 +0000

    Add more unit tests for cards

--- tests/deck/test_card.py
+++ tests/deck/test_card.py
@@ -50,3 +50,37 @@ def test_invalid_value_type():
     with pytest.raises(TypeError) as error_info:
         Card(Suit.DIAMONDS, value="11")
 
+
+def test_card_value_equal():
+    """Test 2 card object with same value and suit are equal"""
+    card_1 = Card(Suit.HEARTS, value=10)
+    card_2 = Card(Suit.HEARTS, value=10)
+    assert card_1 == card_2
+
+
+def test_card_face_equal():
+    """Test 2 card object with same face and suit are equal"""
+    card_1 = Card(Suit.HEARTS, face=Face.KING)
+    card_2 = Card(Suit.HEARTS, face=Face.KING)
+    assert card_1 == card_2
+
+
+def test_card_not_equal_face_or_value():
+    """Test 2 card object with different info are not equal"""
+    card_1 = Card(Suit.HEARTS, value=10)
+    card_2 = Card(Suit.HEARTS, face=Face.KING)
+    assert not (card_1 == card_2)
+
+
+def test_card_face_not_equal():
+    """Test 2 card object with different face aren't equal"""
+    card_1 = Card(Suit.SPADES, face=Face.KING)
+    card_2 = Card(Suit.HEARTS, face=Face.KING)
+    assert not (card_1 == card_2)
+
+
+def test_card_value_and_suit_not_equal():
+    """Test 2 card object with different face aren't equal"""
+    card_1 = Card(Suit.SPADES, value=2)
+    card_2 = Card(Suit.HEARTS, value=10)
+    assert not (card_1 == card_2)
