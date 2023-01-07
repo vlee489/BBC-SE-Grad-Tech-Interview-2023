e183581fea385547b4310f7ac024e8e10b8fe2b1
Vincent Lee <vlee@vlee.me.uk>
2023-01-07 03:26:55 +0000

    Allow users to select number of CPUs to play against

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,14 @@
 from app.interface import Interface
 
-Interface(1, 2)
+# Get number of CPU the player wants to play against
+num_of_cpus = 0
+while True:
+    try:
+        user_input = input("How many cpus do you want to play with? : ")
+        num_of_cpus = int(user_input)
+        break
+    except ValueError:
+        print("What you gave was not a number, try again!")
+# Starts the game
+print("You're player 1")
+Interface(1, num_of_cpus)
